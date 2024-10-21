c9674fcd8e5db771418bbbce2ff9511107c24de4
João Lucas Sampaio <160886470+joaolucassampaio@users.noreply.github.com>
2024-10-21 07:37:29 -0300

    Update automatize.py

--- seleniumWppAutomatize/automatize.py
+++ seleniumWppAutomatize/automatize.py
@@ -5,25 +5,50 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 import time
+import urllib.parse
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
+# Conexão com o MongoDB
+client = MongoClient('mongodb+srv://wppAutomatize:y33CvNoCMyQUQtQF@cluster0.idvq4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db = client["db"]
+colection = db["wppAutomatize"]
+
+# Buscar o documento
+document = colection.find_one({"_id": ObjectId("67155bdc24e4973449641009")})
+
+# Configurações do Selenium
 options = webdriver.ChromeOptions()
 options.add_argument("user-data-dir=C:/Users/jlsam/AppData/Local/Google/Chrome/User Data")
 service = Service("chromedriver.exe")
 driver = webdriver.Chrome(service=service, options=options)
 driver.get("https://web.whatsapp.com/")
 
-wait=WebDriverWait(driver,100)
-target='"Luis Francisco Faesa"'
-message="teste"
-number_of_times=5
+if document:
+    contatos = document["contatos"]
+    
+    for contato in contatos:
+        wait = WebDriverWait(driver, 100)
+        message = "Oi, {}! {}".format(contato['name'], "tá pegando fogo, bicho!")
+        
+        # Codificar a mensagem para URL
+        text = urllib.parse.quote(message)
+        link = f"https://web.whatsapp.com/send?phone={contato['phone']}&text={text}"
+        
+        driver.get(link)
+        time.sleep(5)
+        
+        # Aguardar o carregamento da caixa de mensagem
+        message_box_path = '//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[1]/div/div[1]'
+        message_box = wait.until(EC.presence_of_element_located((By.XPATH, message_box_path)))
+        message_box.send_keys(Keys.ENTER)
+        
+        time.sleep(5)  # Espera um pouco antes de enviar o próximo
 
-contact_path='//span[contains(@title,'+ target +')]'
-contact=wait.until(EC.presence_of_element_located((By.XPATH,contact_path)))
-contact.click()
-message_box_path='//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[1]/div/div[1]'
-message_box=wait.until(EC.presence_of_element_located((By.XPATH,message_box_path)))
-for x in range(number_of_times):
-    message_box.send_keys(message + Keys.ENTER)
-    time.sleep(0.2)
+        # Imprimir informações do contato
+        print(f"Contato:")
+        print(f"  Nome: {contato['name']}")
+        print(f"  Telefone: {contato['phone']}")
 
-time.sleep(10)
+# Fechar o navegador após o uso
+driver.quit()
